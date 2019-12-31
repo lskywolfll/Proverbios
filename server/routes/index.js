b68ca29cfda9d1f485de33e221deff01ea8ada6a
lskywolfll <renealejandrosanchezmorales@gmail.com>
2019-12-31 17:10:52 -0300

    se importa proverbio y se usa en el routeo para la api

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,12 @@
 
 const Usuario = require('./usuario');
 const Login = require('./login');
+const Proverbio = require('./proverbio');
 
 const routes = (server) => {
     server.use('/', Usuario);
     server.use('/', Login);
+    server.use('/', Proverbio);
 };
 
 module.exports = routes;
