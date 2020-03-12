c9389d2f6ee9c704c7a4f7754832adecc224a200
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-03-11 23:17:15 -0300

    se hacen cambios en el usuario de las rutas

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -43,7 +43,7 @@ app.get('/Usuario', verficarToken, (req, res) => {
         })
 });
 
-app.post('/Usuario', [verficarToken, verificarAdmin_Role], (req, res) => {
+app.post('/Usuario', (req, res) => {
 
     const body = req.body;
 
@@ -53,6 +53,7 @@ app.post('/Usuario', [verficarToken, verificarAdmin_Role], (req, res) => {
         // 2- body.propiedad
         nombre: body.nombre,
         email: body.email,
+        apellido: body.apellido,
         // Creacion de una contraseña con una seguridad en base a un algoritmo que indicas cuan robusta sera en este ejemplo es de 10 vueltas para hacerlo mas complejo nivel 10
         password: bcrypt.hashSync(body.password, 10),
         role: body.role
